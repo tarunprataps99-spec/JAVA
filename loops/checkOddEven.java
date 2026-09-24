e1de88510bb7e9ec3dd8922c443569e1810f1517
tarunprataps99-spec <tarunprataps99@gmail.com>
2026-09-24 23:29:53 +0530

    ...

--- loops/checkOddEven.java
+++ loops/checkOddEven.java
@@ -1,5 +1,15 @@
 package loops;
 
 public class checkOddEven {
+
+    public static void main(String[] args) {
+        int number = 10;
+
+        if (number % 2 == 0) {
+            System.out.println(number + " is even.");
+        } else {
+            System.out.println(number + " is odd.");
+        }
+    }
     
 }
